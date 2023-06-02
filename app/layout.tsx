2b12fbb4a2f1ffe9ef9631c18e6405b564186e4f
Nikolai Reed-Larsen <nikolai.reedlarsen@fjordline.com>
2023-06-02 13:27:42 +0200

    add support for styled components

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Metadata } from "next"
 
 import { siteConfig } from "@/config/site"
 import { fontSans } from "@/lib/fonts"
+import StyledComponentsRegistry from "@/lib/registry"
 import { cn } from "@/lib/utils"
 import { SiteHeader } from "@/components/site-header"
 import { TailwindIndicator } from "@/components/tailwind-indicator"
@@ -43,7 +44,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
           <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
             <div className="relative flex min-h-screen flex-col">
               <SiteHeader />
-              <div className="flex-1">{children}</div>
+              <div className="flex-1">
+                <StyledComponentsRegistry>{children}</StyledComponentsRegistry>
+              </div>
             </div>
             <TailwindIndicator />
           </ThemeProvider>
